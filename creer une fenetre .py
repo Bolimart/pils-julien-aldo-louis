7b461845ab2b3ef8785beeb0f7942228ce3cb198
A2D-237 <aldokiza76@gmail.com>
2025-11-12 12:27:56 +0100

    Add files via upload

---
+++ creer une fenetre .py
@@ -0,0 +1,9 @@
+from tkinter import *
+# creer une premiere fenetre de l'application
+window = Tk()
+#personnaliser la fenetre
+window.title("Pils")
+
+
+#afficher
+window.mainloop()
---
+++ tkinter 1.py
@@ -0,0 +1,6 @@
+from tkinter import *
+# creer une premiere fenetre de l'application
+window = Tk()
+
+#afficher
+window.mainloop()
